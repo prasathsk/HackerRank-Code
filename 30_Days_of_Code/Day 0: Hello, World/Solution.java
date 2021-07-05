9581ed51072a0e2c8b058cb84b2c84623a5209db
prasathsk <48730426+prasathsk@users.noreply.github.com>
2021-07-05 20:23:31 +0530

    Hello, World

--- 30_Days_of_Code/Day 0: Hello, World/Solution.java
+++ 30_Days_of_Code/Day 0: Hello, World/Solution.java
@@ -1,1 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+	public static void main(String[] args) {
+    
+    // Create a Scanner object to read input from stdin.
+		Scanner scan = new Scanner(System.in); 
+		
+		// Read a full line of input from stdin and save it to our variable, inputString.
+		String inputString = scan.nextLine(); 
+
+		// Close the scanner object, because we've finished reading 
+		scan.close(); 
+      
+		// Print a string literal saying "Hello, World." to stdout.
+		System.out.println("Hello, World.");
+
+    //print the inputString is stdout.
+    System.out.println(inputString);
+
+	}
+}
